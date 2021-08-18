f5c10ce87607bafccc691cbd7d6ebbd43bc9ef58
Vikassony <48961830+Vikassony@users.noreply.github.com>
2021-08-18 17:48:38 +0530

    Create 70. Climbing Stairs.java

---
+++ 70. Climbing Stairs.java
@@ -0,0 +1,27 @@
+/*
+You are climbing a staircase. It takes n steps to reach the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+Example 1:
+
+Input: n = 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+*/
+class Solution {
+    public int climbStairs(int n) {
+        if(n==1|| n==2)
+            return n;
+        int dp[] = new int[n+1];
+        dp[0]=1;
+        for(int i=1;i<=n;i++){
+            if(i==1)
+                dp[i]=dp[i-1];
+            else
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+}
