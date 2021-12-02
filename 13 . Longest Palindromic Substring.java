15c88e3d0d1925965fe175d882e0a6792cb5785b
Vikassony <48961830+Vikassony@users.noreply.github.com>
2021-12-02 07:56:39 +0530

    Update 13 . Longest Palindromic Substring.java

--- 13 . Longest Palindromic Substring.java
+++ 13 . Longest Palindromic Substring.java
@@ -1,25 +1,37 @@
-class Solution {
-	public String longestPalindrome(String s) {
-		boolean dp[][] = new boolean[s.length()][s.length()];
-		String res = "";
-		for (int g = 0; g < s.length(); g++) {
-			for (int i = 0, j = g; j < s.length(); i++, j++) {
-				if (g == 0) {
-					dp[i][j] = true;
-					res = s.substring(i, j + 1);
-				} else if (g == 1) {
-					if (s.charAt(i) == s.charAt(j)) {
-						dp[i][j] = true;
-						res = s.substring(i, j + 1);
-					}
-				} else {
-					if (s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1] == true) {
-						dp[i][j] = true;
-						res = s.substring(i, j + 1);
-					}
-				}
-			}
-		}
-		return res;
-	}
+class Solution{
+    static String longestPalin(String S){
+        int f=0;
+        int s=0;
+        int min=0;
+        boolean flag=true;
+        boolean dp[][] = new boolean[S.length()][S.length()];
+        for(int g=0;g<dp.length;g++)
+        {
+            boolean flag1=true;
+            for(int i=0,j=g;j<dp.length;i++,j++){
+                if(g==0){
+                    dp[i][j]=true;
+                }
+                else if(g==1){
+                    dp[i][j]=S.charAt(i)==S.charAt(j);
+                    if(dp[i][j] && flag){
+                       f=i;
+                        s=j;
+                        flag=false;
+                    }
+                }
+                else{
+                    if(S.charAt(i)==S.charAt(j) && dp[i+1][j-1]){
+                        dp[i][j]=true;
+                        if(flag1){
+                        f=i;
+                        s=j;
+                        flag1=false;
+                        }
+                    }
+                }
+            }
+        }
+        return S.substring(f,s+1);
+    }
 }
